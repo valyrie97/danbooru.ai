1b6de42c4243878364e7fc9c95b9d8c61fdf9d3b
Marcus <marcus@mandworks.com>
2019-11-07 23:52:18 -0500

    credentials fix?

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,11 @@ import credentials from './../../credentials.js';
 const Danbooru = require('danbooru');
 let post;
 let booru;
-booru = new Danbooru(`https://${credentials.login}:${credentials.api_key}@danbooru.donmai.us`);
+if(credentials && credentials.login) {
+	booru = new Danbooru(`https://${credentials.login}:${credentials.api_key}@danbooru.donmai.us`);
+} else {
+	booru = new Danbooru(`https://danbooru.donmai.us`);
+}
 
 
 booru.posts({ limit: 1, random: true }).then(async (gotPost) => {
